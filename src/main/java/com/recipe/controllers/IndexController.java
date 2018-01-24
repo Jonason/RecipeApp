17d1ef4f49f7ffa5cfc75584da15562a3fd74fc0
Jonason <jonasonho@gmail.com>
2018-01-23 18:43:58 -0800

    load test data to category and unit_of_measure tables during startup. Test CategoryRepository and UnitOfMeasureRepository query methods.

--- src/main/java/com/recipe/controllers/IndexController.java
+++ src/main/java/com/recipe/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package com.recipe.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.recipe.domain.Category;
+import com.recipe.domain.UnitOfMeasure;
+import com.recipe.repositories.CategoryRepository;
+import com.recipe.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping({"", "/", "/index"})
-	public String getIndexPage(){
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+
+		Optional<Category> categoryOptional = categoryRepository.findByName("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByOum("Teaspoon");
+
+		System.out.println("Cat Id is: " + categoryOptional.get().getId());
+		System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
 		return "index";
 	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (name) VALUES ('American');
+INSERT INTO category (name) VALUES ('Italian');
+INSERT INTO category (name) VALUES ('Mexican');
+INSERT INTO category (name) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (oum) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (oum) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (oum) VALUES ('Cup');
+INSERT INTO unit_of_measure (oum) VALUES ('Pinch');
+INSERT INTO unit_of_measure (oum) VALUES ('Ounce');
